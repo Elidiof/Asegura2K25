e895e034bec933aa07c338358c77a24528f11994
Elidiof <elidioff@gmail.com>
2025-05-25 22:41:13 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
---- src/App.jsx
-@@ function App() {
--      </Suspense>
--    </main>
--    <CookieBanner />
--    <Footer />
-+      </Suspense>
-+    </main>
-+    {/*
-+      <CookieBanner />
-+      <Footer />
-+    */}
+import React, { Suspense } from 'react';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+// import { CookieBanner } from './components/CookieBanner'; // Deshabilitado para depuración
+// import Footer from './components/Footer'; // Deshabilitado para depuración
+
+// Componentes cargados perezosamente
+const Home = React.lazy(() => import('./pages/Home'));
+const Seguro = React.lazy(() => import('./pages/Seguro'));
+
+function App() {
+  return (
+    <HelmetProvider>
+      <HashRouter>
+        <main className="container mx-auto p-4">
+          <Suspense fallback={<div className="p-4 text-center">Cargando…</div>}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/seguro/:name" element={<Seguro />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </Suspense>
+        </main>
+        {/*
+          CookieBanner y Footer temporalmente comentados
+          <CookieBanner />
+          <Footer />
+        */}
+      </HashRouter>
+    </HelmetProvider>
+  );
+}
+
+export default App;
