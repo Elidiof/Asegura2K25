74d73ccd8b854902218bd6f82cae372c33137c16
Elidiof <elidioff@gmail.com>
2025-05-27 01:28:18 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
     <header className="bg-blue-900 text-white py-6">
       <div className="container mx-auto text-center px-4">
         <Link to="/">
-          <img src="/logo.png" alt="Asegura2K25" className="h-12 md:h-16 mx-auto" />
+          <img src="/logo.png" alt="Asegura2K25" className="h-[4.5rem] md:h-[6rem] mx-auto" />
         </Link>
       </div>
     </header>
