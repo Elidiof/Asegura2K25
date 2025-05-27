d57b07f47ab97b02cd34ca60e0f5110bf111cbc7
Elidiof <elidioff@gmail.com>
2025-05-28 01:00:16 +0200

    Delete src directory

--- src/components/ui/Card.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export function Card({ children, className = '' }) {
-  return (
-    <div className={`rounded-xl shadow-md p-4 bg-white ${className}`}>
-      {children}
-    </div>
-  )
-}
