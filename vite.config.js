cfee2893f37433ca859537e62234683cc4bfb37b
Elidiof <elidioff@gmail.com>
2025-05-28 01:01:37 +0200

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  // Fuerza que todos los imports de JS/CSS/imagenes sean relativos
-  base: './',
-  plugins: [react()]
-});
