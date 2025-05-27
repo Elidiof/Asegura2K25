3393e327c24f52b1225906922be79c7520989966
Elidiof <elidioff@gmail.com>
2025-05-28 00:28:50 +0200

    Add files via upload

--- src/components/WhatsButton.jsx
+++ src/components/WhatsButton.jsx
@@ -1,17 +1,17 @@
-
 import React from 'react'
+import { FaWhatsapp } from 'react-icons/fa'
 
-export default function WhatsButton() {
+function WhatsButton() {
   return (
     <a
       href="https://wa.me/34658945741"
-      className="fixed bottom-4 right-4 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600"
       target="_blank"
       rel="noopener noreferrer"
+      className="fixed bottom-5 right-5 bg-green-500 hover:bg-green-600 text-white p-3 rounded-full shadow-lg z-50"
     >
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-        <path d="M20.52 3.48A11.93 11.93 0 0012 0C5.37 0 0 5.37 0 12c0 2.11.55 4.16 1.6 5.98L0 24l6.27-1.64A11.94 11.94 0 0012 24c6.63 0 12-5.37 12-12 0-3.19-1.24-6.18-3.48-8.52z" />
-      </svg>
+      <FaWhatsapp size={24} />
     </a>
   )
 }
+
+export default WhatsButton
