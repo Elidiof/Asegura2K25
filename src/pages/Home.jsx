df5199478a88b0a50d9566b93f8c0d00243a9e8c
Elidiof <elidioff@gmail.com>
2025-05-28 12:17:43 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,21 @@
-
 // src/pages/Home.jsx
+import { Helmet } from 'react-helmet-async'
 import productData from '../data/productData'
 import ProductCard from '../components/ProductCard'
-import { Helmet } from 'react-helmet-async'
 
-export default function Home () {
+export default function Home() {
   return (
     <>
       <Helmet>
-        <title>Asegura2K25</title>
+        <title>Asegura2K25 • Seguros a medida</title>
       </Helmet>
 
+      {/* Frase introductoria */}
+      <section className="bg-blue-900 text-white text-center py-12">
+        <h1 className="text-3xl font-bold">Encuentra el seguro que necesitas</h1>
+      </section>
+
+      {/* GRID DE PRODUCTOS */}
       <section className="bg-slate-200 py-16">
         <div className="container mx-auto px-4">
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6">
@@ -19,7 +24,7 @@ export default function Home () {
                 key={key}
                 to={`/seguro/${key}`}
                 icon={prod.icon}
-                title={prod.title}
+                title={prod.title.replace(/^Seguro de\s?/, '')}
               />
             ))}
           </div>
