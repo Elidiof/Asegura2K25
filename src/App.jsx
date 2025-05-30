8b14b8e7ea71a6435829df9c42831175e7a79ba0
Elidiof <elidioff@gmail.com>
2025-05-30 20:30:58 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import React, { Suspense } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 
+import ScrollToTop from './ScrollToTop'
 import Header from './components/Header'
 import Footer from './components/Footer'
-// import { CookieBanner } from './components/CookieBanner' // ← Eliminado porque usamos Cookiebot
+import { CookieBanner } from './components/CookieBanner'
 
 // Lazy-loaded pages
 const Home               = React.lazy(() => import('./pages/Home'))
@@ -14,23 +15,34 @@ const AvisoLegal         = React.lazy(() => import('./pages/AvisoLegal'))
 const PoliticaPrivacidad = React.lazy(() => import('./pages/PoliticaPrivacidad'))
 const PoliticaCookies    = React.lazy(() => import('./pages/PoliticaCookies'))
 
-export default function App () {
+export default function App() {
   return (
     <HelmetProvider>
+      {/* Scroll to top on route change */}
+      <ScrollToTop />
+
       <Header />
 
       <Suspense fallback={<div className="p-8 text-center">Cargando…</div>}>
         <Routes>
+          {/* Landing */}
           <Route path="/" element={<Home />} />
-          <Route path="/seguro/:id" element={<Seguro />} />
+
+          {/* Detalle de producto: /seguro/coche, /seguro/hogar, etc. */}
+          <Route path="/seguro/:name" element={<Seguro />} />
+
+          {/* Textos legales */}
           <Route path="/aviso-legal" element={<AvisoLegal />} />
-          <Route path="/politica-privacidad" element={<PoliticaPrivacidad />} />
-          <Route path="/politica-cookies" element={<PoliticaCookies />} />
+          <Route path="/privacidad"  element={<PoliticaPrivacidad />} />
+          <Route path="/cookies"     element={<PoliticaCookies />} />
+
+          {/* 404 -> inicio */}
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Suspense>
 
       <Footer />
+      <CookieBanner />
     </HelmetProvider>
   )
 }
