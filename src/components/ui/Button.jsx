c2a1080cd6565c50a2694df4ad0c1dc2e27df4cc
Elidiof <elidioff@gmail.com>
2025-06-07 12:23:15 +0200

    Update Button.jsx

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-export function Button({ children, ...props }) {␊
-  return (␊
-    <button␊
-      className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"␊
-      {...props}␊
-    >␊
-      {children}␊
-    </button>␊
+
+export function Button({ children, ...props }) {
+  return (
+    <button
+      className="rounded bg-green-500 px-4 py-2 text-white hover:bg-green-600"
+      {...props}
+    >
+      {children}
+    </button>
   );
 }
