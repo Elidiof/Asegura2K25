4aa5559cb04ba043b15b6e65d0fd475f4fe43aae
Elidiof <elidioff@gmail.com>
2025-06-07 12:09:47 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
-import FooterLinks from './FooterLinks';
+import FooterLinks from "./FooterLinks";
 
 export default function Footer() {
   return (
     <footer className="bg-blue-900 py-6 text-center">
       <FooterLinks />
       <p className="text-xs text-white/70 mt-4">
-        © {new Date().getFullYear()} Asegura2K25. Todos los derechos reservados.
+        © {new Date().getFullYear()} Asegura2K25. Todos los derechos
+        reservados.
       </p>
     </footer>
   );
