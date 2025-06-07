1d3afcc71c6f66c3c972c20feff6a3bfc3a08c1d
Elidiof <elidioff@gmail.com>
2025-06-07 10:12:43 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
 // src/App.jsx
-import React, { Suspense } from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import { HelmetProvider } from 'react-helmet-async'
+import React, { Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 
-import ScrollToTop from './ScrollToTop'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import ScrollToTop from "./ScrollToTop";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 // Lazy-loaded pages
-const Home               = React.lazy(() => import('./pages/Home'))
-const Seguro             = React.lazy(() => import('./pages/Seguro'))
-const AvisoLegal         = React.lazy(() => import('./pages/AvisoLegal'))
-const PoliticaPrivacidad = React.lazy(() => import('./pages/PoliticaPrivacidad'))
-const PoliticaCookies    = React.lazy(() => import('./pages/PoliticaCookies'))
+const Home = React.lazy(() => import("./pages/Home"));
+const Seguro = React.lazy(() => import("./pages/Seguro"));
+const AvisoLegal = React.lazy(() => import("./pages/AvisoLegal"));
+const PoliticaPrivacidad = React.lazy(
+  () => import("./pages/PoliticaPrivacidad"),
+);
+const PoliticaCookies = React.lazy(() => import("./pages/PoliticaCookies"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
 
 export default function App() {
   return (
     <HelmetProvider>
-	  <Header />
+      <Header />
       <ScrollToTop />
 
       <Suspense fallback={<div className="p-8 text-center">Cargando…</div>}>
@@ -25,12 +28,12 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/seguro/:name" element={<Seguro />} />
           <Route path="/aviso-legal" element={<AvisoLegal />} />
-          <Route path="/privacidad"  element={<PoliticaPrivacidad />} />
-          <Route path="/cookies"     element={<PoliticaCookies />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
+          <Route path="/privacidad" element={<PoliticaPrivacidad />} />
+          <Route path="/cookies" element={<PoliticaCookies />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Suspense>
       <Footer />
     </HelmetProvider>
-  )
+  );
 }
