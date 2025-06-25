1090d2b3825e5b8f4215f753ee608106a89bca08
Elidiof <elidioff@gmail.com>
2025-06-25 19:19:38 +0200

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,28 @@
+// tailwind.config.cjs
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
-  theme: { extend: {} },
-  plugins: []
-}
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // Reescribimos la escala azul con negro
+        blue: {
+          50:  '#000000',
+          100: '#000000',
+          200: '#000000',
+          300: '#000000',
+          400: '#000000',
+          500: '#000000',
+          600: '#000000',
+          700: '#000000',
+          800: '#000000',
+          900: '#000000',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
