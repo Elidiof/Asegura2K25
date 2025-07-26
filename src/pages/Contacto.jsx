ddaf09da18c1486d64425b80fdb63f94899c1e7e
Elidiof <elidioff@gmail.com>
2025-07-26 17:56:54 +0200

    Update Contacto.jsx

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -30,7 +30,6 @@ export default function Contacto() {
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
-@@ -50,26 +50,25 @@ export default function Contacto() {
                 viewBox="0 0 24 24"
                 stroke="currentColor"
                 strokeWidth={2}
