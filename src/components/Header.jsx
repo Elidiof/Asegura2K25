3bdb53aa1088a92e2056f0a124d0039f28cd7efa
Elidiof <elidioff@gmail.com>
2025-07-26 17:28:21 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <header className="bg-blue-900 text-white py-6">
-      <div className="container mx-auto text-center px-4">
+    <header className="bg-blue-900 py-6 text-white">
+      <div className="container mx-auto px-4 text-center">
         <Link to="/">
-          <img src="/logo.png" alt="Asegura2K25" className="h-[4.5rem] md:h-[6rem] mx-auto" />
+          <img src="/logo.png" alt="Asegura2K25" className="mx-auto h-[4.5rem] md:h-24" />
         </Link>
       </div>
     </header>
